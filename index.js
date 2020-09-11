074f8571081b8e7c8fcd1c295a6d6a3fb15b98a9
mt <margarettom6@gmail.com>
2020-09-11 22:22:15 +0300

    Fixed search functionality(use axios)

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 
 const input = document.getElementById("input");
-input.addEventListener('keyup', async (input)=> {
+input.addEventListener('keyup', async (event) => {
+	event.preventDefault();
+		const typedString = document.getElementById("input").value;
 	
-	const typedString = document.getElementById("input").value;
-	// const url = "https://spott.p.rapidapi.com/places/autocomplete?q="+input+"&limit=10&skip=0&type=CITY&language=ar&country=US%252CCA",
+		await axios(`https://spott.p.rapidapi.com/places?type=CITY&limit=1&language=ar&skip=0&q=${typedString}`, {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "",
+				"x-rapidapi-key": ""
+			}
+		})
+			.then(response => {
+				let data = response.data[0]
+				// let data = JSON.parse(response)
+				console.log(data);
 
+				
 
-	await fetch(`https://spott.p.rapidapi.com/places/autocomplete?q=${typedString}&limit=10&skip=0&type=CITY&language=ar`, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "spott.p.rapidapi.com",
-		"x-rapidapi-key": "e894934359msh14a1ac1cf2e5dc7p1115b0jsn7009fce07bb0"
-	}
-})
-.then(response => {
-	let data = response.json();
-	console.log(data.id);
-
-const country = document.querySelector('country');
-country.textContent = data.name
-
-})
-.catch(err => {
-	console.log(err);
-});
+			})
+			.catch(err => {
+				console.log(err);
+			});
 })
 
+// https://spott.p.rapidapi.com/places/autocomplete?q=${typedString}&limit=10&skip=0&type=CITY&language=ar
