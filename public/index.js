8c458a512a60cf93f42ba0fb6f5cbcfb2b9ed265
mt <margarettom6@gmail.com>
2020-09-18 22:47:55 +0300

    refactor code into functions

--- public/index.js
+++ public/index.js
@@ -12,56 +12,97 @@ const year = document.querySelector('#year');
 year.textContent = today.getFullYear()
 
 
+// const tommorow = new Date(date)
+// const displayNextday = document.querySelector('#nextDay');
+// displayNextday.textContent = tommorow.toLocaleString('default', { weekday: 'long' })
+
+// let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+// let day = today.getDay()
+// const nextday2 = document.querySelector('#nextDay2');
+// nextday2.textContent = days[day+2]
+// for (let i = day; i>=6; i++){
+
+// }
+
+// date = tommorow.getDate()
+// const nextday2 = document.querySelector('#nextDay2');
+// let next = today.getDay()+1
+// nextday2.textContent = today.toLocaleString('default', { weekday: 'long' })
+// const next = new Date((tommorow.getDate()+2))
+// const nextday2 = document.querySelector('#nextDay2');
+// nextday2.textContent = next.toLocaleString('default', { weekday: 'long' })
+// nextday.textContent = today.getDate() + 1
+// const tommorow = today.getDay()+1
+// const tomorrow = new Date()
+// nextday.textcontent =  tomorrow.setDate(today.getDate() + 1)
+
+
 const input = document.getElementById("input");
+
 input.addEventListener('keyup', async (event) => {
 	event.preventDefault();
 	if (event.keyCode === 13) {
 		const typedString = document.getElementById("input").value;
-		// const apiKey = functions.config().openweather.key;
-
-		await axios(`https://api.openweathermap.org/data/2.5/weather?q=${typedString}&APPID=`, {
-			"method": "GET"
-		})
-			.then(response => {
-				let data = response.data
-				console.log(data);
-
-				const city = document.querySelector('.city');
-				city.textContent = `${data.name},`
-
-				const country = document.querySelector('.country');
-				country.textContent = data.sys.country
-
-				let icon = data.weather[0].icon
-				const img = document.querySelector('#weatherIcon');
-				img.setAttribute('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
-
-				const temp = document.querySelector('.degrees')
-				temp.textContent = `${Math.round(data.main.temp - 273.15)}°`;
-
-				const weather = document.querySelector('.weather');
-				weather.textContent = data.weather[0].description
-
-				const humidity = document.querySelector('#humidityRate')
-				humidity.textContent = `${data.main.humidity}%`
-
-				const cloudRate = document.querySelector('#cloudRate');
-				cloudRate.textContent = `${data.clouds.all}%`
-
-
-				const windSpeed = document.querySelector('#windSpeedRate');
-				windSpeed.textContent = `${data.wind.speed} m/s`
-
-				const pressureRate = document.querySelector('#pressureRate');
-				pressureRate.textContent = `${data.main.pressure} hPa`
-
-			})
-			.catch(err => {
-				console.log(err);
-			});
-
-
-	
+		getDaysForecast(typedString)
 	}
 })
 
+async function getDaysForecast(typedString){
+	await axios(`https://api.openweathermap.org/data/2.5/weather?q=${typedString}&APPID=d72c9198e329c1ee1652b88a716f343f`, {
+		"method": "GET"
+	})
+		.then(async response => {
+			let data = response.data
+			const lat = data.coord.lat
+			const lon = data.coord.lon
+			console.log(data);
+
+			displayDaysForecast(data)
+			
+		})
+		.catch(err => {
+			console.log(err);
+		});
+}
+
+function displayDaysForecast(data) {
+	const city = document.querySelector('.city');
+	city.textContent = `${data.name},`
+
+	const country = document.querySelector('.country');
+	country.textContent = data.sys.country
+
+	let icon = data.weather[0].icon
+	const img = document.querySelector('#weatherIcon');
+	img.setAttribute('src', `http://openweathermap.org/img/wn/${icon}@2x.png`)
+
+	const temp = document.querySelector('.degrees')
+	temp.textContent = `${Math.round(data.main.temp - 273.15)}°`;
+
+	const weather = document.querySelector('.weather');
+	weather.textContent = data.weather[0].description
+
+	const humidity = document.querySelector('#humidityRate')
+	humidity.textContent = `${data.main.humidity}%`
+
+	const cloudRate = document.querySelector('#cloudRate');
+	cloudRate.textContent = `${data.clouds.all}%`
+
+
+	const windSpeed = document.querySelector('#windSpeedRate');
+	windSpeed.textContent = `${data.wind.speed} m/s`
+
+	const pressureRate = document.querySelector('#pressureRate');
+	pressureRate.textContent = `${data.main.pressure} hPa`
+}
+
+
+// //for loop that checks the day and prints out the da
+// for(i = 0; i < 7; i++){
+// 	let day = today.getDay()
+// 	let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// 	for(j = day; j >= 6; j++){
+// 		console.log(days[day]);
+// 	}
+// }
