45848e4c72aad1713f79dda205b887b676a9c9d4
mt <margarettom6@gmail.com>
2020-09-26 17:24:36 +0300

    add notifications

--- public/index.js
+++ public/index.js
@@ -3,14 +3,6 @@ const input = document.getElementById("input");
 const addbtn = document.querySelector('.btn');
 let defferedPrompt;
 
-if('serviceWorker' in navigator){
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-    .then(reg => console.log(reg))
-    .catch(err => console.log(err))
-  })
-}
-
 (function displayDate() {
 	const displayDate = document.querySelector('#date');
 
@@ -36,8 +28,10 @@ async function getDaysForecast(typedString) {
 	})
 		.then(async response => {
 			let data = response.data
+
 			const lat = data.coord.lat
 			const lon = data.coord.lon
+
 			console.log(data);
 
 			displayDaysForecast(data)
@@ -49,33 +43,25 @@ async function getDaysForecast(typedString) {
 }
 
 function displayDaysForecast(data) {
-	const city = document.querySelector('.city');
-	city.textContent = `${data.name},`
-
 	const country = document.querySelector('.country');
-	country.textContent = data.sys.country
-
-	let icon = data.weather[0].icon
+	const city = document.querySelector('.city');
 	const img = document.querySelector('#weatherIcon');
-	img.setAttribute('src', `https://openweathermap.org/img/wn/${icon}@2x.png`)
-
 	const temp = document.querySelector('.degrees')
-	temp.textContent = `${Math.round(data.main.temp - 273.15)}°`;
-
 	const weather = document.querySelector('.weather');
-	weather.textContent = data.weather[0].description
-
 	const humidity = document.querySelector('#humidityRate')
-	humidity.textContent = `${data.main.humidity}%`
-
 	const cloudRate = document.querySelector('#cloudRate');
-	cloudRate.textContent = `${data.clouds.all}%`
-
-
 	const windSpeed = document.querySelector('#windSpeedRate');
-	windSpeed.textContent = `${data.wind.speed} m/s`
-
 	const pressureRate = document.querySelector('#pressureRate');
+	
+	let icon = data.weather[0].icon
+
+	city.textContent = `${data.name},`
+	country.textContent = data.sys.country
+	img.setAttribute('src', `https://openweathermap.org/img/wn/${icon}@2x.png`)
+	temp.textContent = `${Math.round(data.main.temp - 273.15)}°`;
+	weather.textContent = data.weather[0].description
+	humidity.textContent = `${data.main.humidity}%`
+	windSpeed.textContent = `${data.wind.speed} m/s`
 	pressureRate.textContent = `${data.main.pressure} hPa`
 }
 
@@ -102,10 +88,10 @@ function displayWeeksForecast(data) {
 	let weeksIcon = nextDayIcon[0]
 	let weeksForecast = nextforeCast[0]
 
-	for(i = 0; i < 7; i++) {
-	weeksTemp[i].innerText = `${Math.round(data[i].temp.day - 273.15)}°`;
-	weeksIcon[i].setAttribute('src', `https://openweathermap.org/img/wn/${data[i].weather[0].icon}.png`) 
-	weeksForecast[i].innerText = data[i].weather[0].main
+	for (i = 0; i < 7; i++) {
+		weeksTemp[i].innerText = `${Math.round(data[i].temp.day - 273.15)}°`;
+		weeksIcon[i].setAttribute('src', `https://openweathermap.org/img/wn/${data[i].weather[0].icon}.png`)
+		weeksForecast[i].innerText = data[i].weather[0].main
 	}
 }
 
@@ -125,3 +111,45 @@ addbtn.addEventListener('click', event => {
 		defferedPrompt = null;
 	})
 })
+
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('./sw.js')
+			.then(reg => console.log(reg))
+			.catch(err => console.log(err))
+	})
+}
+
+// if (!('Notification' in window)) {
+//   console.log('This browser does not support notifications!');
+// }
+// Notification.requestPermission(status => {
+//   console.log('Notification permission status:', status);
+// });
+
+// if (Notification.permission == 'granted') {
+//   navigator.serviceWorker.getRegistration().then(reg => {
+
+//     const options = {
+// 			body: 'Subscribe to get daily weather updates!',
+// 			icon: './favicon-32x32.png',
+// 			vibrate: [100, 50, 100],
+// 			data: {
+// 				dateOfArrival: Date.now(),
+// 				primaryKey: 1
+// 			},
+// 			actions: [
+// 				{action: 'explore', title: 'Subscribe',
+// 					icon: './favicon-32x32.png'},
+// 				{action: 'close', title: 'Dismiss',
+// 					icon: './favicon-32x32.png'},
+// 			]
+		
+// 			// TODO 5.1 - add a tag to the notification
+		
+// 		};
+
+//     reg.showNotification('Want to receive weather updates?', options);
+//   });
+// }
